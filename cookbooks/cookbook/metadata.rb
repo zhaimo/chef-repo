9d0acf85ac36c0959a6c6726337282097f10170d
Haimo-Zhang <haimo.zhang@ibm.com>
2017-07-01 18:42:03 -0400

    m

--- cookbooks/cookbook/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'cookbook'
-maintainer       'IBM'
-maintainer_email 'haimo.zhang@ibm.com'
-license          'Apache 2.0'
-description      'Installs/Configures cookbook'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
