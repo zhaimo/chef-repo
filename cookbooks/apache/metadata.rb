eaccdf5e88272fe99a724e106786175064a15684
Haimo-Zhang <haimo.zhang@ibm.com>
2017-07-01 18:57:40 -0400

    m

--- cookbooks/apache/metadata.rb
+++ cookbooks/apache/metadata.rb
@@ -18,3 +18,6 @@ chef_version '>= 12.1' if respond_to?(:chef_version)
 # a Supermarket.
 #
 # source_url 'https://github.com/<insert_org_here>/apache'
+depends "chef-client"
+depends "apt"
+depends "ntp"
--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,6 +3,9 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+include_recipe "chef-client"
+include_recipe "apt"
+include_recipe "ntp"
 package "httpd"
 
 service "httpd" do
