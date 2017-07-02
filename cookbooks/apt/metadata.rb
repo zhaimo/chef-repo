70f3c46e7b09ff5cc0b0f354c4e8086e7df97b8d
Haimo-Zhang <haimo.zhang@ibm.com>
2017-07-01 20:04:34 -0400

    m

---
+++ cookbooks/apt/metadata.rb
@@ -0,0 +1,7 @@
+name             'apt'
+maintainer       'IBM'
+maintainer_email 'haimo.zhang@ibm.com'
+license          'Apache 2.0'
+description      'Installs/Configures apt'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ cookbooks/nginx/metadata.rb
@@ -0,0 +1,8 @@
+name             'nginx'
+maintainer       'IBM'
+maintainer_email 'haimo.zhang@ibm.com'
+license          'Apache 2.0'
+description      'Installs/Configures nginx'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+depends "apt"
